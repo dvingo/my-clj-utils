80efb73b12deb19d3f29d4500510ed893908a871
Dan Vingo <dan@danvingo.com>
2021-03-06 12:06:15 -0500

    Storing reitit-routes on components

--- src/main/dv/fulcro_reitit2.cljs
+++ src/main/dv/fulcro_reitit2.cljs
@@ -3,41 +3,24 @@
     [cljs.spec.alpha :as s]
     [clojure.string :as str]
     [com.fulcrologic.fulcro.application :as app]
-    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
+    [com.fulcrologic.fulcro.components :as c]
     [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
     [com.fulcrologic.guardrails.core :refer [>defn => | ? >def]]
+    [dv.fulcro-util :as fu]
     [dv.fulcro-util :as fu]
     [goog.object :as g]
     [reitit.core :as r]
     [reitit.frontend :as rf]
-    [dv.fulcro-util :as fu]
     [reitit.frontend.easy :as rfe]
     [taoensso.timbre :as log]))
-
-;;;; why not just put the state in the fulcro state atom?
-;; it's a valid question - one idea is to swap directly against the
-;; state atom without triggering UI updates.
-
-;    https://book.fulcrologic.com/#_swapping_on_the_state_atom
-;;;;
 
 (defn reitit-router? [x] (satisfies? r/Router x))
 
 (>def ::name keyword?)
-(>def ::segment (s/or :vec (s/coll-of (s/or :s string? :k keyword? :fn fn?)) :fn fn?))
+(>def ::segment (s/coll-of (s/or :s string? :k keyword? :fn fn?) :type vector?))
 (>def ::route-map (s/keys :req-un [::name ::segment] :opt-un [::redirect-to]))
 (>def ::route (s/cat :s string? :r ::route-map))
-
-(comment
-  (let [routes
-        [["/" {:name :root :segment ["tasks"]}]
-         ["/tasks" {:name :tasks :segment ["tasks"]}]
-         ["/calendar" {:segment ["calendar"]}
-          ["" {:name :calendar :segment [(fn [_] "202020")]}]
-          ["/:date" {:name :calendar-date :segment [:date]}]]
-         ["/signup" {:name :signup :segment ["signup"]}]]
-        ]
-    (init-router-state {} routes)))
 
 (>def ::reitit-router reitit-router?)
 (>def ::routes-by-name map?)
@@ -56,14 +39,6 @@
      ::redirect-loop-count
      ::max-redirect-loop-count]))
 
-;::fr/router :router :reitit-router reitit-router
-;[::fr/router :router :routes-by-name] routes-by-name
-;[::fr/router :router :current-fulcro-route] []
-;[::fr/router :router :redirect-loop-count] 0
-;[::fr/router :router :max-redirect-loop-count] max-redirect-loop-count
-
-(defn get-state [s] (if (satisfies? IDeref s) @s s))
-
 (def router-state-keys
   [:reitit-router
    :routes-by-name
@@ -72,31 +47,14 @@
    :redirect-loop-count
    :max-redirect-loop-count])
 
-(>def ::deref-or-map (s/or :m map? :d #(satisfies? IDeref %)))
-(>def ::state-arg (s/or :d #(satisfies? IDeref %) :app app/fulcro-app? :m map?))
+;; leaving so it's easier to swap out how state is stored.
+(>def ::state-arg app/fulcro-app?)
 
-(>defn ->map
-  "Get fulcro-state atom's underlying map.
-  can pass:
-  a map - returns the map
-  an atom - derefs
-  fulcro app - get state-atom and deref"
-  [v]
-  [::state-arg => any?]
-  (when (s/valid? ::state-arg v)
-    (let [[k v] (s/conform ::state-arg v)]
-      (log/info "CONFORMED key: " k)
-      (log/info "CONFORMED: " v)
-      (case k
-        :m v
-        :d @v
-        :app @(::app/state-atom v)))))
-
-(defn router-path
-  ([]
+(>defn router-path
+  ([] [=> (s/tuple qualified-keyword? keyword?)]
    [::router :router])
   ([prop]
-   ;[keyword? => (s/tuple qualified-keyword? keyword? keyword?)]
+   [keyword? => (s/tuple qualified-keyword? keyword? keyword?)]
    [::router :router prop]))
 
 (defn map-vals [f m]
@@ -113,17 +71,37 @@
         (assoc prop-map :path path-str))
       grouped)))
 
-(defn init-router-state
-  "Takes a map - your app state atom's value - assoc-in router state needed to work."
-  [s routes]
-  (let [reitit-router  (rf/router routes)
-        routes-by-name (make-routes-by-name reitit-router)]
+(defn assoc-router-state [s router-state]
+  (let [{:keys [reitit-router routes-by-name current-fulcro-route
+                max-redirect-loop-count
+                redirect-loop-count]} router-state]
     (-> s
       (assoc-in [::router :router :reitit-router] reitit-router)
       (assoc-in [::router :router :routes-by-name] routes-by-name)
-      (assoc-in [::router :router :current-fulcro-route] [])
-      (assoc-in [::router :router :redirect-loop-count] 0)
-      (assoc-in [::router :router :max-redirect-loop-count] *max-redirect-loop-count*))))
+      (assoc-in [::router :router :current-fulcro-route] current-fulcro-route)
+      (assoc-in [::router :router :redirect-loop-count] redirect-loop-count)
+      (assoc-in [::router :router :max-redirect-loop-count] max-redirect-loop-count))))
+
+(defn initial-router-state
+  "Takes a map - your app state atom's value - assoc-in router state needed to work."
+  [routes]
+  (let [reitit-router (rf/router routes)]
+    {:reitit-router           reitit-router
+     :routes-by-name          (make-routes-by-name reitit-router)
+     :current-fulcro-route    []
+     :redirect-loop-count     0
+     :max-redirect-loop-count *max-redirect-loop-count*}))
+
+(comment
+  (let [routes
+        [["/" {:name :root :segment ["tasks"]}]
+         ["/tasks" {:name :tasks :segment ["tasks"]}]
+         ["/calendar" {:segment ["calendar"]}
+          ;; showing default values
+          ["" {:name :calendar :segment [(fn [_] "202020")]}]
+          ["/:date" {:name :calendar-date :segment [:date]}]]
+         ["/signup" {:name :signup :segment ["signup"]}]]]
+    (init-router-state {} routes)))
 
 (defn router-state* [app]
   (-> app ::app/runtime-atom deref (get-in (router-path))))
@@ -165,40 +143,45 @@
       (fn [s] (cond-> s (fn? s) (apply [match-data])))
       segment)))
 
-(defn current-fulcro-route [router]
-  (-> router router-state :current-fulcro-route))
+(>defn current-fulcro-route
+  [app]
+  [app/fulcro-app? => vector?]
+  (-> app router-state :current-fulcro-route))
 
 ;; todo support query-params also, by merging
 
-(defn fulcro-segment-from-match [{:keys [path-params] :as m}]
+(defn fulcro-segment-from-match
+  [{:keys [path-params query-params] :as m}]
   (let [fulcro-segments (construct-fulcro-segments m)
-        ;; fill in any dynamic path segments with their values
+        ;; fill in any dynamic path segments with their values from the reitit match
         target-segment  (mapv (fn [part]
-                                (cond->> part (keyword? part) (get path-params)))
+                                (if (keyword? part)
+                                  (or (part path-params) (part query-params))
+                                  part))
                               fulcro-segments)]
+    (log/info "Target segment: " target-segment)
     target-segment))
 
-
-(defn set-route-state! [app prop v]
+(defn set-router-state! [app prop v]
   (swap! (::app/runtime-atom app)
     (fn [s] (assoc-in s (router-path prop) v))))
 
-(defn update-route-state!
+(defn update-router-state!
   [app prop f]
   (swap! (::app/runtime-atom app)
     (fn [s] (update-in s (router-path prop) f))))
 
-(defn handle-redirect [app route params]
+(defn handle-redirect
+  [app route params]
   (let [params (params)
         {:keys [max-redirect-loop-count redirect-loop-count]} (router-state app)]
     (do (log/info "redirecting to: " route " with params " params)
         (if (> redirect-loop-count max-redirect-loop-count)
           (do
             (log/error (str "The route " route " hit the max redirects limit: " max-redirect-loop-count))
-            (set-route-state! app :redirect-loop-count 0))
+            (set-router-state! app :redirect-loop-count 0))
           (do
-            (update-route-state! app :redirect-loop-count inc)
-            #_(vswap! (router-state app :redirect-loop-count) inc)
+            (update-router-state! app :redirect-loop-count inc)
             (js/setTimeout #(rfe/replace-state route params)))))))
 
 ;; Looks like the first match comes in as nil when init! is called.
@@ -210,24 +193,23 @@
   [app m]
   (log/info "on-match called with: " m)
   (let [{:keys [reitit-router]} (router-state app)
-        m          (or m {:path (g/get js/location "pathname")})
-        {:keys [path]} m
+        {:keys [path] :as m} (or m {:path (g/get js/location "pathname")})
         has-match? (rf/match-by-path reitit-router path)]
+
     (log/info "router, match: " m)
-    (if-not has-match?
+    (if has-match?
+      (if-let [{:keys [route params]} (get-in m [:data :redirect-to])]
+        (handle-redirect app route params)
+
+        ;; Path matched
+        (let [fulcro-segment (fulcro-segment-from-match m)]
+          (log/info "Invoking Fulcro change route with " fulcro-segment)
+          (set-router-state! app :current-fulcro-route fulcro-segment)
+          (dr/change-route! app fulcro-segment)))
       ;; unknown page, redirect to root
       (do
         (log/info "No fulcro route matched the current URL, changing to the default route.")
-        (js/setTimeout #(rfe/push-state :default)))
-
-      ;; route has redirect
-      (if-let [{:keys [route params]} (get-in m [:data :redirect-to])]
-        (handle-redirect app route params)
-        (let [fulcro-segment       (fulcro-segment-from-match m)
-              current-fulcro-route (router-state app :current-fulcro-route)]
-          (log/info "Invoking Fulcro change route with " fulcro-segment)
-          (set-route-state! app :current-fulcro-route fulcro-segment)
-          (dr/change-route! app fulcro-segment))))))
+        (js/setTimeout #(rfe/push-state :default))))))
 
 (defn current-route [this]
   (some-> (dr/current-route this this) first keyword))
@@ -273,13 +255,12 @@
 
   ([app route-key params]
    [app/fulcro-app? keyword? map? => any?]
-   (let [routes-by-name (router-state app :routes-by-name )
-           {:keys [name] :as route} (get routes-by-name route-key)]
-       (when-not (route=url? app route-key params)
-         (log/info "Changing route to : " route)
-         (log/info "push state : " name " params: " params)
-         (rfe/push-state name params)))))
-;;;;;;;;;;; trying storing state in t
+   (let [routes-by-name (router-state app :routes-by-name)
+         {:keys [name] :as route} (get routes-by-name route-key)]
+     (when-not (route=url? app route-key params)
+       (log/info "Changing route to : " route)
+       (log/info "push state : " name " params: " params)
+       (rfe/push-state name params)))))
 
 (>defn start-router!
   "Starts reitit router listening to URL changes."
@@ -289,22 +270,51 @@
     (log/info "Starting router: " reitit-router)
     (rfe/start! reitit-router (partial on-match app) {:use-fragment false})))
 
-(defn register-router!
-  "swap!s initial state for the router into the fulcro runtime-atom."
+;; todo support merging in the new routes instead of overwriting.
+
+(defn register-routes!
+  "swap!s in state for the router into the fulcro runtime-atom."
   [app routes]
   (let [{::app/keys [runtime-atom]} app]
     (swap! runtime-atom
-      (fn [s] (init-router-state s routes)))))
+      (fn [s]
+        (if (router-registered? app)
+          (let [curr-route-state (router-state app)]
+            (assoc-router-state s
+              (fu/deep-merge (initial-router-state routes) curr-route-state)))
+          (assoc-router-state s (initial-router-state routes)))))))
+
+(>defn gather-routes
+  "Constructs a data structure of routes as used by reitit from a Fulcro Router
+  assembles reitit routes from dv.fulcro-reitit/route data specified on each route target."
+  [fulcro-router]
+  [dr/router? => vector?]
+  (let [{:keys [router-targets]} (c/component-options fulcro-router)]
+    (reduce
+      (fn [acc t]
+        (if-let [{::keys [route]} (c/component-options t)]
+          (let [f (first route)]
+            (log/info "route: " route)
+            (cond
+              (string? f) (conj acc route)
+              (vector f) (into acc route)
+              :else
+              (throw (js/Error. (str (c/component-name t) " has " ::route "specified, but is in invalid format: " (pr-str route))))))
+          (log/warn (str "Route target " (c/component-name t) " has no path info specified."))))
+      []
+      router-targets)))
+
+;; it would be nice to not lose state in hot reloads
+;; so instead of having register-routes! using init-router-state
+(>defn register-fulcro-router!
+  "Takes a fulcro app and a dr/defrouter component. Gathers reitit route data from the
+  route-targets on the fulcro router and registers a reitit router within the fulcro application."
+  [app fulcro-router]
+  [app/fulcro-app? dr/router? => any?]
+  (let [routes (gather-routes fulcro-router)]
+    (log/info "Registering reitit routes: " routes)
+    (register-routes! app routes)))
 
 (defn register-and-start-router! [app routes]
-  (register-router! app routes)
+  (register-routes! app routes)
   (start-router! app))
-
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-" I still think this setup would work if I store the router state on the application
-and not in the fulcro state-atom. - The fulcro app needs to be created at load time because
-
-it needs to be used in will-enter (r/fulcro-segment SPA :my-route-name)
-"
-;;
