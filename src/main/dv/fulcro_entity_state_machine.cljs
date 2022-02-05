f344c9a2c6cdea46176f02a931de513dfd5e1920
Daniel Vingo <dan@danvingo.com>
2022-02-05 12:05:31 -0500

    add on-success handler for fulcro form state machine

--- src/main/dv/fulcro_entity_state_machine.cljs
+++ src/main/dv/fulcro_entity_state_machine.cljs
@@ -19,6 +19,7 @@
 
 (defn assoc-active-state
   [env]
+  ;(log/info "assoc active state sf actor -> comp " (sm/asm-value env ::sm/actor->component-name))
   (let [cur-state (get-active-state env)]
     (-> env
       (sm/assoc-aliased :machine-state cur-state)
@@ -36,16 +37,20 @@
 
 (defn handle-submit
   [{::sm/keys [event-data] :as env}]
-  (let [{:keys [entity remote-mutation mutation target target-xform on-reset-mutation on-reset creating?]} event-data
+  (let [{:keys [entity remote-mutation mutation target target-xform on-success on-reset-mutation on-reset creating?]} event-data
         form-cls      (sm/actor-class env :actor/form)
         form-instance (actor->inst :actor/form env)
         item-cls      (sm/actor-class env :actor/new-item)
         remote-entity (fu/elide-client-only-values entity)]
+    ;(log/info "sf actor -> comp " (sm/asm-value env ::sm/actor->component-name))
+    ;(log/info "ITEM CLAS: " item-cls)
+    ;(log/info "form CLAS: " form-cls)
     (when mutation (c/transact! form-instance `[(~mutation)]))
     (-> env
       (cond->
         (some? on-reset-mutation) (sm/store :on-reset-mutation on-reset-mutation)
-        (some? on-reset) (sm/store :on-reset on-reset))
+        (some? on-reset) (sm/store :on-reset on-reset)
+        (some? on-success) (sm/store :on-success on-success))
       (sm/assoc-aliased :server-msg "")
       (cond->
         (some? remote-mutation)
@@ -86,18 +91,20 @@
      {:event/success {::sm/handler
                       (global-handler
                         (fn [{{:keys [form-cls entity target target-xform creating?]} ::sm/event-data :as env}]
-                          (log/info "SUBMIT SUCCESS")
-                          ;; todo scroll to top of window
-                          (-> env
-                            (sm/apply-action
-                              #(apply merge/merge-component
-                                 (into [% form-cls (cond-> entity (some? target-xform) target-xform)] (fu/map->vec target))))
-                            (sm/apply-action
-                              #((if creating? fs/pristine->entity* fs/entity->pristine*) % (sm/actor->ident env :actor/form)))
-                            (sm/apply-action #(fs/clear-complete* % (sm/actor->ident env :actor/form)))
-                            (sm/assoc-aliased :server-msg "Success")
-                            (sm/set-timeout :clear-msg-timer :event/reset {:entity entity} 2000)
-                            (sm/activate :state/success))))}
+                          (let [on-success (sm/retrieve env :on-success)]
+                            (log/info "SUBMIT SUCCESS")
+                            ;; todo scroll to top of window
+                            (-> env
+                              (sm/apply-action
+                                #(apply merge/merge-component
+                                   (into [% form-cls (cond-> entity (some? target-xform) target-xform)] (fu/map->vec target))))
+                              (sm/apply-action
+                                #((if creating? fs/pristine->entity* fs/entity->pristine*) % (sm/actor->ident env :actor/form)))
+                              (sm/apply-action #(fs/clear-complete* % (sm/actor->ident env :actor/form)))
+                              (cond-> on-success (sm/apply-action on-success))
+                              (sm/assoc-aliased :server-msg "Success")
+                              (sm/set-timeout :clear-msg-timer :event/reset {:entity entity} 2000)
+                              (sm/activate :state/success)))))}
       :event/failed  {::sm/handler
                       (global-handler
                         (fn [env]
@@ -133,21 +140,23 @@
 (defn submit-entity!
   "target - fulcro target path
   target-xform - a pure function that takes the entity and transforms it in some way before it is passed to target.
+  on-success - optional state mutation helper from state-map -> state-map (called via uism/apply-action)
+  on-reset - a callback that will be passed the :entity passed to this function after it has been successfully saved.
+              This is invoked via js/setTimeout and it is expected that this function will side-effect.
   creating? - if true the form will be reset to the pristine state after successfully creating the entity
               if false the form pristine state will be updated to be the new entity state.
   "
-
-  [this {:keys [machine remote-mutation mutation on-reset-mutation on-reset entity target target-xform creating?]}]
+  [this {:keys [machine remote-mutation mutation on-reset-mutation on-reset on-success entity target target-xform creating?]}]
   (assert machine) (assert entity)
   (when (and remote-mutation mutation)
     (throw (fu/error "You can only pass a mutation or a remote-mutation, but not both.")))
-
   ;(log/info "submit-entity! state:" (sm/get-active-state this machine))
   (sm/trigger! this machine :event/submit
     (cond->
       {:entity            entity
        :on-reset          on-reset
        :on-reset-mutation on-reset-mutation
+       :on-success        on-success
        :target            target
        :target-xform      target-xform
        :creating?         creating?}
@@ -155,6 +164,10 @@
       mutation (assoc :mutation mutation))))
 
 (defn begin! [this id new-entity]
+  (log/info "starting form machine with new entity: " new-entity)
+  (when-not
+    ((:ident (c/component-options new-entity)) {})
+    (throw (js/Error. (str "The return component you passed must have a singleton ident or be a component instance: " (pr-str new-entity)))))
   (sm/begin! this form-machine id
     {:actor/form     this
      :actor/new-item new-entity}))
